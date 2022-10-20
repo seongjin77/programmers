e81f430bd3d7545cae54674626d6e3c769f5cfae
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-10-20 18:17:59 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = [];
+    
+    for(let i=1; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            answer.push(i)
+            if(n / i !== i) {
+            answer.push(n/i)
+        }
+        }
+    }
+    answer.sort((a,b)=> a-b);
+    
+    return answer;
+}
