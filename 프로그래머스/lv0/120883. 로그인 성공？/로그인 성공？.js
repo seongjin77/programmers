8e777ce4949fb0fc494e63f7942e4bccb74df7e3
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-10-24 21:20:49 +0900

    [level 0] Title: 로그인 성공?, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -1,22 +1,31 @@
 function solution (id_pw, db) {
-    let result ='fail';
+    let result ='';
     db.map(data => {
         let checkedId = id_pw[0] === data[0]
         let checkedPw = id_pw[1] === data[1]
        if(checkedId && checkedPw) {
-            result = 'login'
+           return result = 'login'
        } else if (checkedId && !checkedPw) {
-           result = 'wrong pw'
+           return result = 'wrong pw'
+       } else {
+           return result = 'fail'
        }
     })
     return result
 }
 
-function solution(id_pw, db) {
-    const [ userId, userPw ] = id_pw;
-    for (const [ dbId, dbPw ] of db) {
-        if (userId === dbId && userPw === dbPw) return "login";
-        else if (userId === dbId && userPw !== dbPw) return "wrong pw";
-    }
-    return "fail";
+function solution (id_pw, db) {
+    let result ='';
+    db.map(data => {
+        let checkedId = id_pw[0] === data[0]
+        let checkedPw = id_pw[1] === data[1]
+       if(checkedId && checkedPw) {
+           return result = 'login'
+       } else if (checkedId && !checkedPw || !checkedId && checkedPw) {
+           return result = 'wrong pw'
+       } else {
+           return result = 'fail'
+       }
+    })
+    return result
 }
