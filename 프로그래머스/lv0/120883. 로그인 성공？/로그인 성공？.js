df3486cdbf875b733e5aaa5d7b610cb44ee19ca9
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-10-24 17:52:45 +0900

    [level 0] Title: 로그인 성공?, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.js
@@ -3,9 +3,9 @@ function solution (id_pw, db) {
     let result ='';
     
     db.map(data => {
-       if(id_pw[0] ===  data[0] && id_pw[1] === data[1]) return result = 'login';
-       else if(id_pw[0] ===  data[0] && id_pw[1] !== data[1]) return result ='wrong pw';
-        else if(id_pw[0] !==  data[0] && id_pw[1] !== data[1]) result = 'fail';
+       if(id_pw[0] ===  data[0] && id_pw[1] === data[1])   result = 'login';
+       else if(id_pw[0] !==  data[0] && id_pw[1] !== data[1])  result = 'fail';
+        else result = 'wrong pw';
     })
     
     
