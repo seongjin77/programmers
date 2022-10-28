be841f77566c58c69cc5e205eb9583ed75362dff
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-10-28 20:56:07 +0900

    [level 0] Title: 모스부호 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    var answer = '';
+    let change = letter.split(' ');
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    
+    for(let i=0; i<change.length; i++){
+        answer += morse[change[i]]
+    }
+    
+    return answer;
+}
