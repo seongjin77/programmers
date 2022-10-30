38a93e30644604d1ff320ae7e40775985b0fd671
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-10-31 00:53:46 +0900

    [level 0] Title: k의 개수, Time: 10.27 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.js
@@ -2,9 +2,18 @@ function solution(i, j, k) {
     var answer = 0;
     
     for(let t= i; t<= j; t++){
-       answer += t.toString().split('').filter(v => v === k.toString()).length;
+       const m = new RegExp(`${k}`,'g')
+       let test = t.toString().match(m);
+       
+        if(test){
+            answer += test.length;
+        }
+        
+       
+        
     }
 
+    
     
     
     return answer;
