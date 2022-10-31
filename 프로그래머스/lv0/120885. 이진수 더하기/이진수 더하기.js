b60040e01928b6b9a466920739f241e220a2e789
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-10-31 21:38:22 +0900

    [level 0] Title: 이진수 더하기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -0,0 +1,12 @@
+function solution(bin1, bin2) {
+   
+    
+    let num1 = parseInt(bin1,2)
+    let num2 = parseInt(bin2,2)
+    let num3 = (num1 + num2).toString(2)
+    
+  
+    
+    
+    return num3
+}
