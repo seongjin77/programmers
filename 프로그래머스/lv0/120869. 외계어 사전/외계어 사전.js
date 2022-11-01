bcddf3da0beb3f659f52215a83ab4efae3ea6732
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-11-01 13:16:38 +0900

    [level 0] Title: 외계어 사전, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -1,16 +1,13 @@
 function solution(spell, dic) {
-    var answer = 2;
+    var answer = 0;
     
-    for(let i=0; i<dic.length; i++){
-        let count =0;
-        for(let j=0; j<spell.length; j++){
-           if(dic[i].includes(spell[j])){
-                count += 1
-           }
-            
-            if(count === spell.length) answer = 1
-        }
-    }
+    answer = dic.filter(v => {
+       return spell.every(t => v.includes(t))
+    }).length ? 1 : 2
+    
+ 
+    
+  
     
     return answer;
 }
