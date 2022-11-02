8e23b984f71b629684aac79c863623aab71df10a
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-11-02 14:26:41 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.js
@@ -2,7 +2,7 @@ function solution(numbers, k) {
     let idx = 0
     for(let i = 1; i < k; i++){
         idx += 2;
-        if(idx > numbers.length){
+        if(idx >= numbers.length){
             idx %= numbers.length
         }
     }
