d3becb754c6e415b3f76755f45f7dc2cdeb486e5
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-11-08 22:32:37 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -1,13 +1,8 @@
+
 function solution(score) {
-    // 등수를 담을 object 생성
-    const dict = {}
-    // 현재의 점수를 평균 별 오름차순으로 정렬 한 후 자료 사전에 입력
-    const rank = score
-        .map(([a,b]) => (a+b)/2)
-        .sort((a,b) => b-a)
-        .forEach((a,i) => dict[a] === undefined ? dict[a] = i+1 : null)
-    console.log(dict)
-    
-    
-    return score.map(([a,b]) => dict[(a+b)/2])
+  return score.map((el) => {
+    return (
+      score.filter((v) => (v[0] + v[1]) / 2 > (el[0] + el[1]) / 2).length + 1
+    );
+  });
 }
