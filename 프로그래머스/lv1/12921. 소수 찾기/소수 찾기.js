06d9a0fd380c4353537e623c1f0a4489a067596b
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2022-11-23 23:32:18 +0900

    [level 1] Title: 소수 찾기, Time: 67.42 ms, Memory: 44.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -0,0 +1,20 @@
+function solution(n) {
+ 
+    
+    
+    function 소수(num){
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+    for(let i = 2 ; i * i <= n; i++){
+        if(arr[i]){
+            for(let j = i * i; j <= n; j+=i){
+                arr[j] = false;
+            }
+        }
+    }
+
+    return arr;
+}
+    
+    
+    return 소수(n).filter(x=> x === true ).length;
+}
