f8d5dfbf27abb065d3e418ccd190671f14ac841e
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2023-02-08 21:18:18 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,10 +1,9 @@
 function solution(a, b) {
- 
     let sum = 0;
-    //1. 배열 a,b의 같은 위치의 요소를 곱하여 더해라
-    for(let i=0; i<a.length; i++){
+    
+    for(let i=0; i< a.length; i++){
         sum += a[i] * b[i]
     }
-    
+   
     return sum;
 }
