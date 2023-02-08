ac80a97e67dc437e5f50acb50bd6bf73de89163c
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2023-02-08 21:14:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,9 +1,11 @@
 function solution(numbers) {
     var answer = 0;
     
-    for(let i=0; i<10; i++){
-        if(!numbers.includes(i)) answer += i; 
-    } 
-   
+    for(let i=1; i<= 9; i++){
+        
+        if(!numbers.includes(i)) answer += i
+    }
+    
+ 
     return answer;
 }
