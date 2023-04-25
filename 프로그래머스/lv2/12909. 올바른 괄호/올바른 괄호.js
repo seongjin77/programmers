ecf5efe4e85ccfbdfa375ee14723f18715645c67
seongjin77 <96804958+seongjin77@users.noreply.github.com>
2023-04-25 23:02:31 +0900

    [level 2] Title: 올바른 괄호, Time: 7.59 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,12 @@
+function solution(s) {
+  let check = 0;
+
+  for (let round of s) {
+    
+    round === "(" ? check++ : check--;
+
+    if (check < 0) return false;
+  }
+
+  return check === 0 ? true : false;
+}
